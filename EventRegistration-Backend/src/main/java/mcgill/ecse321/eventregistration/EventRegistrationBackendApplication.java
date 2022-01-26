953877e6217b04e658ee0e740dcbce83d55d45b7
Adam Kazma <adkazma2@gmail.com>
2022-01-26 15:40:52 -0500

    testfixes again

--- EventRegistration-Backend/src/main/java/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
+++ EventRegistration-Backend/src/main/java/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -1,19 +1,19 @@
 package mcgill.ecse321.eventregistration;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@SpringBootApplication
+@RestController
+@SpringBootApplication
 public class EventRegistrationBackendApplication {
 
   public static void main(String[] args) {
-    //SpringApplication.run(EventRegistrationBackendApplication.class, args);
+    SpringApplication.run(EventRegistrationBackendApplication.class, args);
   }
 
-  //@RequestMapping("/")
+  @RequestMapping("/")
   public String greeting(){
     return "Hello world!";
   }
